f074bbce05c635a2a987f65217cbddcc209e86d6
yandong <ereddate@gmail.com>
2015-04-27 09:54:08 +0800

    Create kim.list.js

---
+++ kim.list.js
@@ -0,0 +1,23 @@
+;
+(function() {
+	jQuery.kim.modelExtend({
+		list: function(elem) {
+			var self = this;
+			self.config.handle[jQuery(elem).attr("ng-list")].call(self, function(data) {
+				var tmpl = jQuery(elem).html(),
+					html = [];
+				jQuery(elem).data("tmpl", tmpl);
+				jQuery.each(data, function(i, obj) {
+					var temp = tmpl;
+					temp = jQuery.kim.tmpl(obj, temp);
+					html.push(temp);
+				});
+				var newitem = jQuery(html.join(''));
+				jQuery(elem).html(newitem).show();
+				self.add(elem);
+			}, self);
+
+			return this;
+		}
+	})
+})()
